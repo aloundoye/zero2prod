43da2baeb8be273c7fb365645cc8c5de4f8a03e8
Alassane Ndoye <aloutndoye.an@gmail.com>
2026-06-23 16:22:18 +0000

    3.3.3 Implementing the Health Check Handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{http::StatusCode, routing::get, Router};
+use axum::response::IntoResponse;
+
+async fn health_check() -> impl IntoResponse{
+    StatusCode::OK
 }
+
+#[tokio::main]
+async fn main() {
+    // tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/health_check", get(health_check));
+
+    let addr = "0.0.0.0:8000";
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    println!("Listening on http://{}", addr);
+    axum::serve(listener, app).await.unwrap();
+}
+
